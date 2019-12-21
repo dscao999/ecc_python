383ad18a5ea514371a1570f6ed80d089003abc47
Dashi Cao <dscao999@gmail.com>
2019-12-21 20:55:21 +0800

    Encrypt/Decrypt Private keys

--- audiorand.py
+++ audiorand.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 #
+import sys
 import sounddevice as sd
 import hashlib
+import base64
+
+def bin2str_b64(byte_str):
+    return base64.b64encode(byte_str)
+
+def str2bin_64(cstr):
+    return base64.b64decode(cstr)
 
 class SndRnd:
     def __init__(self):
@@ -11,10 +19,28 @@ class SndRnd:
         self.rawsm = sd.RawInputStream(device=self.phone['name'], dtype='int16')
         self.hash = hashlib.new('sha256')
 
-    def ecc256_keygen(self, secs):
+    def ecc256_random(self, secs):
         frames = secs *int(self.phone['default_samplerate'])
         self.rawsm.start()
         noise = self.rawsm.read(frames)
         self.rawsm.stop()
         self.hash.update(bytes(noise[0]))
         return self.hash.digest()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        secs = int(sys.argv[1])
+    else:
+        secs = 1
+    rndsrc = SndRnd()
+    rndnum = rndsrc.ecc256_random(secs)
+    b64rnd = bin2str_b64(rndnum)
+    fname = ''
+    if len(sys.argv) > 2:
+        fname = sys.argv[2]
+    if len(fname) == 0:
+        print(b64rnd.decode("utf-8"))
+    else:
+        fo = open(fname,'wb')
+        fo.write(b64rnd)
+        fo.close()
--- eccop.py
+++ eccop.py
@@ -4,19 +4,43 @@ import os
 import sys
 import tkinter as tk
 import tkinter.filedialog as filedialog
+from Crypto.Cipher import AES
+import Crypto.Random as random
 import audiorand
+import binascii
 
 class KeyFile(tk.Frame):
     def generate_key(self):
-        keystr = self.sndrnd.ecc256_keygen(5)
-        print(len(keystr))
-##        self.keyf.config(state='normal')
-##        self.keyf.delete(0, 'end')
-##        self.keyf.insert(0, keystr)
-##        self.keyf.config(state='readonly')
-        return keystr
+        keystr = self.sndrnd.ecc256_random(5)
+        self.keylist.append(keystr)
+        print(audiorand.bin2str_b64(keystr))
 
-    def __init__(self, parent=None, fname=None):
+    def load_key(self):
+        fname = self.fname_str.get()
+        if not os.path.exists(fname):
+            return
+        self.keylist = []
+        mh = audiorand.hashlib.new('ripemd160')
+        mh.update(self.passwd_str.get().encode('utf-8'))
+        passwd = mh.digest()
+        aes = AES.new(passwd[:16], AES.MODE_ECB)
+        ifp = open(self.fname_str.get(), 'rb')
+        cip = ifp.read(48)
+        while cip:
+            pla = aes.decrypt(cip)
+            crc32 = binascii.crc32(pla[:44])
+            crcf = int.from_bytes(pla[44:], 'big')
+            if crc32 != crcf:
+                print("Invalid Pass Word!")
+                cip = ifp.read(48)
+                continue
+            keystr = pla[:32]
+            self.keylist.append(keystr)
+            print(audiorand.bin2str_b64(keystr))
+            cip = ifp.read(48)
+        ifp.close()
+
+    def __init__(self, parent=None, fname=None, width=32):
         super().__init__(parent)
         self.mfont = ('courier', 16, 'bold')
         self.pack(side=tk.TOP, expand=tk.YES, fill=tk.BOTH)
@@ -25,18 +49,6 @@ class KeyFile(tk.Frame):
         f1.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
         f2 = tk.Frame(self)
         f2.pack(side=tk.BOTTOM, expand=tk.YES, fill=tk.X)
-
-        row1 = tk.Frame(f2)
-        row1.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
-        keylab = tk.Label(row1, text='Key Hash File:', font=self.mfont)
-        keylab.pack(side=tk.LEFT)
-
-        hname = fname.rsplit('.', 1)[0]
-        self.hname_str = tk.StringVar()
-        self.hname_str.set(fname)
-        keyh = tk.Entry(row1, width=32, textvariable=self.hname_str)
-        keyh.config(font=self.mfont, state='readonly')
-        keyh.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
 
         f1_1 = tk.Frame(f1)
         f1_1.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
@@ -49,9 +61,7 @@ class KeyFile(tk.Frame):
         keylab.pack(side=tk.LEFT)
         self.fname_str = tk.StringVar()
         self.fname_str.set(fname)
-        keyf = tk.Entry(row1, width=32, textvariable=self.fname_str)
-        keyf.bind('<Return>', self.fname_set)
-        keyf.bind('<FocusOut>', self.fname_set)
+        keyf = tk.Entry(row1, width=width, textvariable=self.fname_str)
         keyf.config(font=self.mfont)
         keyf.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
 
@@ -59,41 +69,67 @@ class KeyFile(tk.Frame):
         row2.pack(side=tk.BOTTOM, expand=tk.YES, fill=tk.X)
         passlab = tk.Label(row2, text='  Passwd:', font=self.mfont)
         passlab.pack(side=tk.LEFT)
-        passtext = tk.Entry(row2, show="*", width=32)
+        self.passwd_str = tk.StringVar()
+        self.passwd_str.set('')
+        passtext = tk.Entry(row2, show="*", textvariable=self.passwd_str, width=width)
         passtext.config(font=self.mfont)
         passtext.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
 
         row3 = tk.Frame(f1_2)
         row3.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
-
         tk.Button(row3, text="Select Key File", command=self.select_file,
                 font=self.mfont, width=16).pack(side=tk.TOP)
-        tk.Button(row3, text="Generate New Key", font=self.mfont,
-                width=16).pack(side=tk.BOTTOM)
+
+        row4 = tk.Frame(f1_2)
+        row4.pack(side=tk.BOTTOM, expand=tk.YES, fill=tk.X)
+        tk.Button(row4, text="Generate New Key", font=self.mfont,
+                width=16, command=self.generate_key).pack(side=tk.LEFT)
+        tk.Button(row4, text="Save Key", font=self.mfont,
+                width=16, command=self.save_key).pack(side=tk.RIGHT)
+        tk.Button(row4, text="Load Key", font=self.mfont,
+                width=16, command=self.load_key).pack()
 
         self.sndrnd = audiorand.SndRnd()
-
-
-    def fname_set(self, sv):
-        fname = self.fname_str.get()
-        hname = fname.rsplit('.', 1)[0]
-        self.hname_str.set(hname+'.pub')
+        self.keylist = []
 
     def select_file(self):
-        fname = filedialog.askopenfilename(parent=self, multiple=False,
-                title='Select Key File')
+        fname = filedialog.asksaveasfilename(parent=self, title='Select Key File',
+                filetypes=(("secret key", "*.pri"), ("all files", "*.*")))
         if len(fname) != 0:
             self.fname_str.set(fname)
-            hname = fname.rsplit('.', 1)[0]
-            self.hname_str.set(hname+'.pub')
+
+
+    def save_key(self):
+        mh = audiorand.hashlib.new('ripemd160')
+        mh.update(self.passwd_str.get().encode('utf-8'))
+        passwd = mh.digest()
+        aes = AES.new(passwd[:16], AES.MODE_ECB)
+        ofp = open(self.fname_str.get(), 'wb')
+        for keystr in self.keylist:
+            appstr = self.sndrnd.ecc256_random(1)
+            plain = keystr + appstr[:12]
+            crc32 = binascii.crc32(bytes(plain))
+            plain += crc32.to_bytes(4, 'big')
+            scrtext = aes.encrypt(plain)
+            ofp.write(scrtext)
+        ofp.close()
+
+    def mexit(self):
+        print("I'm exiting!")
+        sys.exit()
 
 root = tk.Tk()
 root.title(sys.argv[0])
 
-fname='ecc256_key.dat'
+fname=os.getcwd() + '/ecc256_key.pri'
 if len(sys.argv) > 1:
     fname = sys.argv[1]
 
-keyfile = KeyFile(root, fname)
+width = len(fname)
+if (width < 32): width = 32
+
+keyfile = KeyFile(root, fname, width)
+
+root.protocol('WM_DELETE_WINDOW', keyfile.mexit)
 
 root.mainloop()
