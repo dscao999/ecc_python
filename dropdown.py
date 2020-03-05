f6e810ce5cbe557eb6af3238e568b6e76a37249b
Dashi Cao <dscao999@gmail.com>
2020-03-05 15:07:17 +0800

    Add Mariadb Connection

--- dropdown.py
+++ dropdown.py
@@ -1,6 +1,15 @@
 import tkinter as tk
+import tkinter.messagebox as mesgbox
 import sys
 import mysql.connector as mariadb
+
+mariadb_config = {
+        'user': 'dscao',
+        'host': 'localhost',
+        'database': 'electoken'
+}
+
+tables = ["vendors", "etoken_cat", "etoken_type", "sales"]
 
 class DropDown(tk.OptionMenu):
     def __init__(self, parent, optlist, dv):
@@ -8,18 +17,35 @@ class DropDown(tk.OptionMenu):
         self.vari.set(optlist[dv])
         super().__init__(parent, self.vari, *optlist)
 
-    def getv():
-        vstr = self.vari.get()
-        return vstr
+    def get_choice(self):
+        return self.vari.get()
 
+
+def show_vid():
+    vid = mydrop.getv()
+    mesgbox.showerror("Info", vid)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title(sys.argv[0])
     mfont = ('courier', 16, 'bold')
-    mylist = ["Sunday", "Monday", "Tuesday", "Wensday", "Thursday", "Friday", "Saturday"]
+
+    cnx = mariadb.connect(**mariadb_config)
+    cursor = cnx.cursor()
+    vendor_query = ("SELECT * from vendors")
+    cursor.execute(vendor_query)
+    mylist = []
+    for (vid, name, descp) in cursor:
+        mylist.append(name)
+    cursor.close()
+
     mydrop = DropDown(root, mylist, 0);
-    mydrop.config(width=10, font=mfont);
-    mydrop.pack()
+    mydrop.config(width=20, font=mfont);
+    mydrop.pack(side=tk.TOP)
+
+    mbut = tk.Button(root, text="Get ID", width=20, command=show_vid);
+    mbut.config(font=mfont)
+    mbut.pack(side=tk.BOTTOM)
+
 
     root.mainloop()
